85378386ad9de54ac20012f05ee55ca01b15f78f
Vee-Mukoma <wanguii159@gmail.com>
2026-07-07 23:17:47 +0300

    feat: add procedure to process transactions

---
+++ database/process_transactions_procedure.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `salary_managament_system`.`Process Transaction`(
+	IN p_staff_id INT,
+    IN p_payslip_id INT,
+    IN p_amount_paid DECIMAL(10,2),
+    IN p_payment_method VARCHAR(100),
+    IN p_reference_no VARCHAR(100)
+)
+BEGIN
+	 INSERT INTO Transactions (staff_id, payslip_id, amount_paid, payment_date, payment_method, status, reference_no)
+    VALUES (p_staff_id, p_payslip_id, p_amount_paid, NOW(), p_payment_method, 'Completed', p_reference_no);
+END $$
+
+DELIMITER ;
